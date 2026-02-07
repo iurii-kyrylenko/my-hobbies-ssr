9106ea5083203c419274cc4e7c79cab31b344700
iurii-kyrylenko <yuriproject@gmail.com>
2026-02-07 22:57:26 +0200

    Prevend double-fetching of the 1st page

--- src/routes/paging.tsx
+++ src/routes/paging.tsx
@@ -1,3 +1,14 @@
+// Summary of the Lifecycle:
+//
+// 1. Server-Side: The loader prefetches the data, ensuring the HTML
+//    sent to the browser is fully populated with your first page of users.
+//
+// 2. Hydration: TanStack Start seamlessly transfers that cache state
+//    to the browser.
+//
+// 3. Client-Side: useInfiniteQuery sees the "fresh" data in the cache
+//    and waits until the staleTime expires (or you scroll to trigger
+//    a new page) before making another network request.
 
 import { infiniteQueryOptions, useInfiniteQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
@@ -20,6 +31,14 @@ export const usersQueryOptions = () =>
         getNextPageParam: (lastPage) =>
             lastPage.info.page < 10 ? lastPage.info.page + 1 : undefined,
         initialPageParam: 1,
+        // The default staleTime is 0. This means that as soon
+        // as the client-side useInfiniteQuery mounts, it sees
+        // the data in the cache but immediately marks it as stale
+        // and triggers a background refetch to ensure the data is
+        // up-to-date. This effectively causes the double-fetching
+        // of the 1st page both by server and client.
+        // So we overwrite the defaul behaviour:
+        staleTime: 1000 * 60, // Data remains fresh for 1 minute.
     });
 
 export const Route = createFileRoute("/paging")({
@@ -53,7 +72,10 @@ function RouteComponent() {
                 onClick={() => fetchNextPage()}
                 disabled={!hasNextPage || isFetchingNextPage}
             >
-                {isFetchingNextPage ? 'Loading...' : hasNextPage ? 'Load More' : 'Nothing more to load'}
+                {isFetchingNextPage
+                    ? "Loading..."
+                    : hasNextPage ? "Load More" : "Nothing more to load"
+                }
             </button>
         </div>
     );
